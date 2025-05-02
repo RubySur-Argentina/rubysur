c09a710f2a374699dfe33df4862d0e15a77beeb7
Ariel Juodziukynas <arieljuod@gmail.com>
2025-05-01 23:03:56 -0300

    WIP

--- js/app.js
+++ js/app.js
@@ -250,7 +250,7 @@ const bindAboutUsImages = () => {
   const originalAlt = picture.dataset.originalAlt;
 
   const resetImage = () => {
-    pictureWrapper.classList = "pictureWrapper";
+    pictureWrapper.classList = "pictureWrapper textBox";
     image.alt = originalAlt;
   };
 
@@ -258,10 +258,10 @@ const bindAboutUsImages = () => {
 
   // cambio main on hover
   pictureWrapper.addEventListener("mouseenter", () => {
-    pictureWrapper.classList = "pictureWrapper alt";
+    pictureWrapper.classList = "pictureWrapper textBox alt";
   });
   pictureWrapper.addEventListener("click", () => {
-    pictureWrapper.classList = "pictureWrapper alt";
+    pictureWrapper.classList = "pictureWrapper textBox alt";
   });
   pictureWrapper.addEventListener("mouseleave", resetImage);
 
